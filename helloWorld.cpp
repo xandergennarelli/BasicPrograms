ed2fb04f42a22b98d27b8df6bd475bcb0e3c2a2b
xandergennarelli <47370352+xandergennarelli@users.noreply.github.com>
2020-08-28 12:32:12 -0500

    Add files via upload

---
+++ helloWorld.cpp
@@ -0,0 +1,25 @@
+/*
+* Author: Xander Gennarelli
+* Assignment Title: Hello World
+* Assignment Description: A program to print "Hello World. :-)" to the screen.
+* Due Date: 8/26/2020
+* Date Created: 8/24/2020
+* Date Last Modified: 8/24/2020
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Hello World. :-)\n" << endl;
+
+  cout << "*** Start Extra Credit ***\n" << endl;
+
+  cout << ")-: .dlroW olleH\n" << endl;
+
+  cout << "*** End Extra Credit ***" << endl;
+
+  return 0;
+}
