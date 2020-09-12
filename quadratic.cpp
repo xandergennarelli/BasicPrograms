dba05df916d06e72e88fc1a6b3bba7cc92f9514f
Xander Gennarelli <xander.gennarelli@gmail.com>
2020-09-12 17:26:01 -0500

    fixed

--- quadratic.cpp
+++ quadratic.cpp
@@ -12,22 +12,25 @@
 *           Creates a, b, and c to hold their counter-parts in the equation.
 *           Creates d  and numRoots to hold the dicriminant and the
 *                 number of roots.
-*           Creates userIn to hold the users exact input.
-*           Creates index as a placeholder for extracting values from userIn.
 *           Creates root1 and root2 to hold the roots of the quadratic.
+*           Creates vertX and vertY to hold the coordinates of the vertex of
+*                 the equation.
 * Input:
-*           User inputs the values a, b, and c delimited by a comma and a space.
-*           The values of a, b, and c are extracted from the string and placed
-*                 in their respective variables.
+*           User inputs the values a, b, and c.
 * Process:
-*           The discriminant is calculated and used to find the number of roots.
+*           The discriminant is calculated and used to find the number of
+*                 roots.
 *           The roots are calculated if they exist.
 *           The roots are reordered, if necessary, to ensure root1 is less than
 *                 root2.
+*           The x and y coordinates of the vertex are calculated.
 * Output:
-*           Asks the user to input a, b, and c.
 *           Prints the roots if they exist or "NO REAL ROOTS" if they are not
 *                 real. If the roots are identical, only one is printed.
+*           Prints two lines of whitespace.
+*           Prints start extra credit.
+*           Prints the vertex of the function.
+*           Prints the end extra credit.
 * Assumptions:
 *           It is assummed that the user will correctly input data.
 *           It is assummed that the user only wants to solve one equation at a
@@ -43,25 +46,12 @@ using namespace std;
 int main(){
 
   //Data Abstraction
-  int a, b, c, d, numRoots, index;
-  double root1, root2;
-  string userIn;
+  int a, b, c, d, numRoots;
+  double root1, root2, vertX, vertY;
+  //string userIn;
 
   //Input
-  cout << "Please input a, b, and c :: ";
-  getline(cin, userIn);
-  cout << userIn << endl;
-
-  index = userIn.find(',');
-
-  a = stoi(userIn.substr(0, index));
-  userIn = userIn.substr(index + 2);
-  index = userIn.find(',');
-
-  b = stoi(userIn.substr(0, index));
-  userIn = userIn.substr(index + 2);
-
-  c = stoi(userIn);
+  cin >> a >> b >> c;
 
   //Process
   d = pow(b, 2) - (4 * a * c);
@@ -74,7 +64,6 @@ int main(){
   else{
     numRoots = 0;
   }
-
 
   if(numRoots > 0){
     root1 = static_cast<double>((b * -1) + sqrt(d))
@@ -99,7 +88,19 @@ int main(){
     cout << " " << fixed << setprecision(2) << root2;
   }
 
-  cout << endl;
+  cout << "\n" << endl;
+
+  //Extra Credit
+  cout << "\n*** Start Extra Credit ***\n" << endl;
+
+  vertX = static_cast<double>(-1 * b) / static_cast<double>(2 * a);
+  vertY = a * pow(vertX, 2.0) + static_cast<double>(b) * vertX
+          + static_cast<double>(c);
+
+  cout << "Vertex = ("<< fixed << setprecision(2) << vertX << ", " << vertY
+       << ")" << endl;
+
+  cout << "\n*** End Extra Credit ***" << endl;
 
   return 0;
 }
