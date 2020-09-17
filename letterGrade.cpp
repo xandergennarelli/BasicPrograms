18332191a7eb160d741855ac6afd5a0e43590b28
Xander Gennarelli <xander.gennarelli@gmail.com>
2020-09-17 01:22:10 -0500

    finished

--- letterGrade.cpp
+++ letterGrade.cpp
@@ -7,13 +7,14 @@
 *                         mode.
 * Due Date: 9/18/2020
 * Date Created: 9/16/2020
-* Date Last Modified: 9/16/2020
+* Date Last Modified: 9/17/2020
 */
 /*
 * Data Abstraction:
 *           Creates scores[] to hold each score given by the user.
-*           Creates avg, min, max, median, and mode to hold the respective
+*           Creates avg, min, max, and median to hold the respective
 *                 calculated values.
+*           Creates mode[] to hold possible modes if they exist.
 *           Creates grade to hold the calculated letter grade.
 *           Creates count and maxCount to aid in finding the mode.
 * Input:
@@ -22,7 +23,7 @@
 *           The average is calculated and used to find the letter grade.
 *           The scores are sorted from least to greatest.
 *           The min, max, and median scores are found.
-*           The mode is found, if it exists.
+*           The modes are found, if they exist.
 * Output:
 *           Asks the user to enter 5 test scores.
 *           If one score is above 100 or below 0, an error message is printed.
@@ -31,22 +32,27 @@
 *           Start extra credit is printed.
 *           The minimum grade, maximum grade, and median grade are printed.
 *           The mode is printed if it exists, otherwise "No Mode" is printed.
+*                 If there are two modes, they are both printed.
 *           End extra credit is printed.
 * Assumptions:
+*           It is assumed that the user will input correctly.
+*           It is assumed that the user wants to test exactly 5 scores.
 */
 #include <iostream>
 #include <iomanip>
 #include <cmath>
+#include <string>
 using namespace std;
 
 int main(){
   //Data Abstraction
-  double scores[5], avg, min, max, median, mode;
+  double scores[5], avg, min, max, median;
+  double mode[2] = {-1, -1};
   char grade;
   int count, maxCount;
 
   //Input
-  cout << "Please enter 5 test scores:\n" << endl;
+  cout << "Please enter 5 test scores:" << endl;
   cin >> scores[0] >> scores[1] >> scores[2] >> scores[3] >> scores[4];
   for(int i = 0; i < 5; i++){
     if(scores[i] < 0.0 || scores[i] > 100.0){
@@ -56,8 +62,8 @@ int main(){
   }
 
   //Echo print
-  cout << fixed << setprecision(2) << scores[0] << "\n" << scores[1] << "\n"
-       << scores[2] << "\n" << scores[3] << "\n" << scores[4] << endl;
+  cout << "\n" << fixed << setprecision(2) << scores[0] << "\n" << scores[1]
+       << "\n" << scores[2] << "\n" << scores[3] << "\n" << scores[4] << endl;
 
   //Process
   avg = (scores[0] + scores[1] + scores[2] + scores[3] + scores[4]) / 5.0;
@@ -83,7 +89,7 @@ int main(){
   cout << "Grade = " << grade << endl;
 
   //Extra Credit
-  cout << "\n*** Start Extra Credit ***\n" << endl;
+  cout << "\n\n*** Start Extra Credit ***\n" << endl;
 
   for(int i = 0; i < 5; i++){
     for(int j = 0; j < 4; j++){
@@ -98,7 +104,6 @@ int main(){
   max = scores[4];
   median = scores[2];
 
-  mode = -1;
   maxCount = 1;
   for(int i = 0; i < 5; i++){
     count = 0;
@@ -109,18 +114,25 @@ int main(){
     }
     if(count > maxCount){
       maxCount = count;
-      mode = scores[i];
+      mode[0] = scores[i];
+    }
+    else if(count == maxCount && maxCount > 1 &&
+            !(fabs(mode[0] - scores[i]) < 0.00001)){
+      mode[1] = scores[i];
     }
   }
 
   cout << "Min = " << min << endl;
   cout << "Max = " << max << endl;
   cout << "Median = " << median << endl;
-  if(mode == -1){
+  if(mode[0] == -1){
     cout << "Mode = No Mode" << endl;
   }
+  else if(mode[1] > -1){
+    cout << "Mode = " << mode[0] << " " << mode[1] << endl;
+  }
   else{
-    cout << "Mode = " << mode << endl;
+    cout << "Mode = " << mode[0] << endl;
   }
 
   cout << "\n*** End Extra Credit ***" << endl;
