c4b1164be210db0699d737c02cfcddbabd27c34b
Xander Gennarelli <xander.gennarelli@gmail.com>
2020-09-18 14:24:57 -0500

    better data

--- dataRead.cpp
+++ dataRead.cpp
@@ -1,24 +1,45 @@
+#include <iostream>
+#include <string>
 #include <fstream>
-
+#include <cstdlib>
 using namespace std;
 
-int main(){
-  string fileName;
-  ifstream inData;
-  int sum = 0, count = 0, a;
+int main()
+{
+    string fileName;
+    ifstream inData;
+    int sum = 0, count = 0, n;
+    double average;
 
-  cout << "Enter a file name:: " << endl;
-  cin >> fileName;
+    //cout << "Enter File Name: ";
 
-  inData.open(fileName);
-  if(!inData){
-    cout << "Error: File Not Open";
-    exit(1);
-  }
+    //cin >> fileName;
 
-  while(inData >> a){
-    count++;
-    sum += a;
-  }
-  return 0;
+    //inData.open(fileName.c_str());
+    inData.open("myData.txt");
+
+    //This only works in C++ 11.
+    //Older looks like:
+    //inData.open(fileName.c_str())
+    //Always test the connection of this file opening.
+
+    //Files always hold true if open.
+    if(!inData) {
+        cout << "ERROR: File not open - " << fileName << endl;
+        exit(1);
+    }
+
+    //Use this way of reading files.
+
+    while(inData >> n) {
+        count++;
+        sum += n;
+    }
+
+    average = static_cast<double>(sum) / count;
+
+    cout << "Count   = " << count << endl;
+    cout << "Sum     = " << sum << endl;
+    cout << "Average = " << average << endl;
+    return 0;
 }
