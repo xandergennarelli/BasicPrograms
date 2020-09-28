a737a465503bb6f605698c38e495f4dfac5a60c3
Xander Gennarelli <xander.gennarelli@gmail.com>
2020-09-28 18:04:34 -0500

    guessing game

--- guessingGame.cpp
+++ guessingGame.cpp
@@ -2,101 +2,77 @@
 * Author: Xander Gennarelli
 * Assignment Title: Guessing Game
 * Assignment Description:
-*
+*           A program that will ask a user for two integers and choose a random
+*                 number between those integers. The user will then guess the
+*                 generated number while the program tells the user if they are
+*                 guessing too high or low. The number of guesses are then
+*                 reported.
 * Due Date: 9/28/2020
 * Date Created: 9/25/2020
-* Date Last Modified: 9/25/2020
+* Date Last Modified: 9/28/2020
 */
 /*
 * Data Abstraction:
-*
+*           Creates n1 and n2 for user input.
+*           Creates answer to hold the random number generated.
+*           Creates guess to hold user guesses.
+*           Creates count to record the number of guesses.
 * Input:
-*
+*           User inputs two integers.
+*           User inputs a guess number until they are correct.
 * Process:
-*
+*           The user input re-ordered to so n1 is less than n2 if necessary.
+*           rand() is seeded with time and then used to generate a random value
+*                within the range set by the user.
+*           The users guesses are counted.
 * Output:
-*
+*           The user is asked for two integers.
+*           The input is echo printed.
+*           The user is asked for a guess.
+*           The guess is echo printed.
+*           If the guess is greater than the answer, Too High is printed.
+*           If the guess is less than the answer, Too Low is printed.
+*           Once the user guesses correctly, a message is printed with the
+*                 number of guesses.
 * Assumptions:
-*
+*           It is assumed that the user will input correctly.
+*           It is assumed that the user will eventually guess correctly.
+*           It is assumed that input needs to be echo printed.
 */
-
-#include <fstream>
+#include <iostream>
 #include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main(){
   //DATA ABSTRACTION
-  int n1, n2, rand, guess, count = 0;
+  int n1, n2, answer, guess, count = 0;
 
-  //INPUT
+  //INPUT / PROCESS
   cout << "Please enter 2 integers: ";
-  cin << n1 << n2;
-  cout << n1 << " " << n2 << endl;
+  cin >> n1 >> n2;
+  cout << n1 << " " << n2 << endl; //Echo Print
 
   if(n1 > n2){
     swap(n1, n2);
   }
+
+  srand(time(0));
+  answer = (rand() % (n2 - n1 + 1)) + n1;
+  do {
+    cout << "Enter guess: ";
+    cin >> guess;
+    cout << guess; //Echo Print
+
+    if(guess > answer) cout << " - Too High" << endl;
+    if(guess < answer) cout << " - Too Low" << endl;
+
+    count++;
+  }while(guess != answer);
+
+  //OUTPUT
+  cout << "\nCorrect, it took you " << count
+       << " tries to guess my number." << endl;
+
+  return 0;
 }
-
-
-
-
-/*
-
-Please enter 2 integers: 1 10
-I'm thinking of a number between 1 and 10.
-Enter guess: 1 - Too Low
-Enter guess: 2 - Too Low
-Enter guess: 3 - Too Low
-Enter guess: 4 - Too Low
-Enter guess: 5 - Too Low
-Enter guess: 6 - Too Low
-Enter guess: 7 - Too Low
-Enter guess: 8 - Too Low
-Enter guess: 9
-Correct, it took you 9 tries to guess my number.
-
-
-Please enter 2 integers: -10 10
-I'm thinking of a number between -10 and 10.
-Enter guess: 4 - Too Low
-Enter guess: 3 - Too Low
-Enter guess: -3 - Too Low
-Enter guess: -6 - Too Low
-Enter guess: 5 - Too Low
-Enter guess: -2 - Too Low
-Enter guess: 1 - Too Low
-Enter guess: -4 - Too Low
-Enter guess: 8
-Correct, it took you 9 tries to guess my number.
-
-
-Please enter 2 integers: -10 -5
-I'm thinking of a number between -10 and -5.
-Enter guess: 4 - Too High
-Enter guess: 3 - Too High
-Enter guess: -3 - Too High
-Enter guess: -6 - Too High
-Enter guess: 5 - Too High
-Enter guess: -2 - Too High
-Enter guess: 1 - Too High
-Enter guess: -4 - Too High
-Enter guess: 8 - Too High
-Enter guess: -1 - Too High
-Enter guess: 7 - Too High
-Enter guess: -7 - Too High
-Enter guess: -9 - Too High
-Enter guess: -10
-Correct, it took you 14 tries to guess my number.
-
-
-Please enter 2 integers: 22 -18
-I'm thinking of a number between -18 and 22.
-Enter guess: 22 - Too High
-Enter guess: 10 - Too Low
-Enter guess: 14 - Too Low
-Enter guess: -11 - Too Low
-Enter guess: 18
-Correct, it took you 5 tries to guess my number.
-
-*/
