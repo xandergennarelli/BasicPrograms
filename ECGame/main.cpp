6691f72d6095aa38dab88efc6b4f38b80e2514e1
Xander Gennarelli <xander.gennarelli@gmail.com>
2020-10-07 17:38:26 -0500

    i spent way to long on this what the hell

--- ECGame/main.cpp
+++ ECGame/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 #include "SDL_Plotter.h"
 using namespace std;
 
 int main(int argc, char** argv){
-  SDL_Plotter g(1000, 1000);
+  SDL_Plotter g(1000, 1000, true);
   char key;
+  int rdelta = 1, bdelta = 1, gdelta = 1, red = 1, blue = 0, green = 0, tmp;
+  srand(time(0));
 
   while(!g.getQuit()){
     if(g.kbhit()){
@@ -12,7 +16,50 @@ int main(int argc, char** argv){
     }
     for(int y = 0; y < g.getRow(); y++){
       for(int x = 0; x < g.getCol(); x++){
-        g.plotPixel(x, y, 255, 0, 0);
+        g.plotPixel(x, y, red, blue, green);
+      }
+      if(y % 600 == 0){
+        if(!blue && !green){
+          red += rdelta;
+          if(red == 255){
+            rdelta = -1;
+          }
+          if(red == 0){
+            rdelta = 1;
+            blue = 1;
+          }
+        }
+        if(!red && !green){
+          blue += bdelta;
+          if(blue == 255){
+            bdelta = -1;
+          }
+          if(blue == 0){
+            bdelta = 1;
+            green = 1;
+          }
+        }
+        if(!red && !blue){
+          green += gdelta;
+          if(green == 255){
+            gdelta = -1;
+          }
+          if(green == 0){
+            gdelta = 1;
+          }
+        }
+        if((!red && !green && !blue) || (red && green && blue)){
+          red += rdelta;
+          blue += rdelta;
+          green += rdelta;
+          if(red == 255){
+            rdelta = -1;
+          }
+          if(red == 0){
+            rdelta = 1;
+            red = 1;
+          }
+        }
       }
     }
     g.update();
@@ -20,3 +67,5 @@ int main(int argc, char** argv){
 
   return 0;
 }
+
+// cd /home/xander/Documents/CPP/BasicPrograms/ECGame/ && clang++ main.cpp SDL_Plotter.cpp -std=c++11 -g -lglfw -lGL -lGLU -lSDL2 -lSDL2_mixer -I/usr/include/SDL2 && ./a.out
