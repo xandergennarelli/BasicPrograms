b360158c6b6acaec5d8246d18d75a10fec455ca3
Xander Gennarelli <xander.gennarelli@gmail.com>
2020-10-11 22:47:53 -0500

    some work

--- ECGame/main.cpp
+++ ECGame/main.cpp
@@ -2,14 +2,20 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <array>
 #include "SDL_Plotter.h"
 using namespace std;
 
-enum Color {WHT, BLK, RED, GRN, BLU, YEL, MAG};
+enum Color {WHT, BLK, RED, GRN, CYA, YEL, GRY, MAG};
+const int PIXEL_SIZE = 10;
 
 int main(int argc, char** argv){
-  SDL_Plotter g(1000, 1000, true);
+  SDL_Plotter g(800, 1000, true);
   char key;
+
 
   while(!g.getQuit()){
     if(g.kbhit()){
@@ -32,8 +38,37 @@ void draw(SDL_Plotter g, int x, int y, Color c){
     case BLK: g.plotPixel(x, y, 0, 0, 0); break;
     case RED: g.plotPixel(x, y, 255, 25, 25); break;
     case GRN: g.plotPixel(x, y, 25, 255, 25); break;
-    case BLU: g.plotPixel(x, y, 25, 25, 255); break;
+    case CYA: g.plotPixel(x, y, 25, 25, 255); break;
     case YEL: g.plotPixel(x, y, 255, 255, 25); break;
+    case GRY: g.plotPixel(x, y, 64, 64, 64); break;
     case MAG: g.plotPixel(x, y, 255, 0, 255); break;
   }
+}
+
+Color ** initSprite(SDL_Plotter g, string name){
+  ifstream in;
+  string line;
+  stringstream ss;
+  int w, h, val;
+  in.open(name);
+  if(!in){
+    cout << "Error: Bad Sprite Data File Name :: " << name << endl;
+    exit(1);
+  }
+
+  getline(in, line);
+  ss.str(line);
+  ss >> w >> h;
+
+  Color sprite[h][w];
+  for(int i = 0; i < h; i++){
+    getline(in, line);
+    ss.clear();
+    ss.str(line);
+    for(int j = 0; j < w; j++){
+      ss >> val;
+      sprite[i][j] = static_cast<Color>(val);
+    }
+  }
+  return sprite;
 }
