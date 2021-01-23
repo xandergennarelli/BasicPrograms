6a32088baf77abbb57aed52dce24b433c848bf95
Xander Gennarelli <xander.gennarelli@gmail.com>
2021-01-22 19:19:05 -0600

    new semester

---
+++ pointers1.cpp
@@ -0,0 +1,26 @@
+/**
+-  file: pointers1.cpp
+-  author:  Prof. Matthew Aars
+-  course: CSI 1440
+-  assignment: Lab 0
+-  due date: 8/25/2016
+-
+-  Date Modified: 7/9/2020
+-     - Lab changed for zyBooks
+-  Date Modified: 8/25/2014
+-    - File Created
+-
+-  Testing the reference operator
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int x = 5;
+
+    cout << &x << “ is the address of x” << endl;
+    cout << x << “ is the value stored in x” << endl;
+
+    return 0;
+}
